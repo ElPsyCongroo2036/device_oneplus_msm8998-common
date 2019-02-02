d0578d98e5a4f5e31dd70f2c696759dec23c6fac
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-02-02 12:38:21 +0100

    msm8998-common: Replace LiveDisplay implementation by HIDL services
    * Move DisplayModeControl into a livedisplay service, and also add PictureAdjustment impl from https://github.com/LineageOS/android_hardware_lineage_livedisplay.
    
    * Based on the sdm845 implementation from Danny Baumann.
      https://github.com/LineageOS/android_device_oneplus_sdm845-common/commit/4203f3ec180ea65c74ed79d7d9dc87485537998c
    
    * @Hlcpereira: Squash of https://github.com/LineageOS/android_device_oneplus_msm8998-common/commit/725b77221b9811df8ae3bcecc28f8bd55f569352 and https://github.com/LineageOS/android_device_oneplus_msm8998-common/commit/b3e1818539cf8439b1ee744d3853203b553ae52d
    
    Change-Id: Ib39e4646e5fb5aca73d4ebc54e841f859ae0c0d1
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

--- common.mk
+++ common.mk
@@ -278,7 +278,11 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.oneplus_8998
+    android.hardware.light@2.0-service.oneplus_msm8998
+
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service.oneplus_msm8998
 
 # Media
 PRODUCT_COPY_FILES += \
